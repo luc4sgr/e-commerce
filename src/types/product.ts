6f347cf22371cf605df86e9df1a0f0f0fb0877a5
luc4sgr <23.lucasdoliveira@gmail.com>
2025-03-25 06:14:33 -0300

    Add Product interface to define product structure and types

---
+++ src/types/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: string | number; 
+    stock: number;
+    image_url: string;
+    created_at: string;
+    updated_at: string;
+  }
